dff317ae7b0897baed91d0d603f013801b02b50f
김은찬 <164847037+eunchan2815@users.noreply.github.com>
2025-07-30 10:33:21 +0900

    fix:: detail height
    TODO: animation Test 중

--- Projects/Component/Source/Component/Detail/RuinsDetailView.swift
+++ Projects/Component/Source/Component/Detail/RuinsDetailView.swift
@@ -27,136 +27,140 @@ public struct RuinsDetailView: View {
                 .foregroundColor(.gray.opacity(0.3))
                 .padding(.top, 8)
             
-            HStack(alignment: .top, spacing: 14) {
-                VStack(alignment: .leading, spacing: 16) {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("유적지 탐험")
-                            .font(.headline(.bold))
-                            .foreground(LegacyColor.Common.white)
+            ScrollView {
+                HStack(alignment: .top, spacing: 14) {
+                    VStack(alignment: .leading, spacing: 16) {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("유적지 탐험")
+                                .font(.headline(.bold))
+                                .foreground(LegacyColor.Common.white)
+                            
+                            Text("#\(data.ruinsId)")
+                                .font(.caption1(.medium))
+                                .foreground(LegacyColor.Label.alternative)
+                            
+                            Text(data.name)
+                                .font(.headline(.bold))
+                                .foreground(LegacyColor.Label.normal)
+                        }
                         
-                        Text("#\(data.ruinsId)")
-                            .font(.caption1(.medium))
-                            .foreground(LegacyColor.Label.alternative)
+                        HStack(spacing: 6) {
+                            ForEach(1...5, id: \.self) { _ in
+                                HStack(spacing: 0) {
+                                    Image(icon: .leftStar)
+                                        .resizable()
+                                        .frame(width: 7, height: 13)
+                                    
+                                    Image(icon: .rightStar)
+                                        .resizable()
+                                        .frame(width: 7, height: 13)
+                                }
+                            }
+                            .foreground(LegacyColor.Primary.normal)
+                            
+                            Text("( 302 )")
+                                .font(.caption2(.medium))
+                                .foreground(LegacyColor.Fill.alternative)
+                        }
                         
-                        Text(data.name)
-                            .font(.headline(.bold))
-                            .foreground(LegacyColor.Label.normal)
-                    }
-                    
-                    HStack(spacing: 6) {
-                        ForEach(1...5, id: \.self) { _ in
-                            HStack(spacing: 0) {
-                                Image(icon: .leftStar)
-                                    .resizable()
-                                    .frame(width: 7, height: 13)
+                        LegacyDivider()
+                        
+                        HStack(spacing: 20) {
+                            VStack(alignment: .leading) {
+                                Text("탐험자 수")
+                                    .font(.caption2(.regular))
+                                    .foreground(LegacyColor.Label.alternative)
                                 
-                                Image(icon: .rightStar)
-                                    .resizable()
-                                    .frame(width: 7, height: 13)
+                                Text("200001명")
+                                    .font(.body2(.bold))
+                                    .foreground(LegacyColor.Label.normal)
+                            }
+                            
+                            VStack(alignment: .leading) {
+                                Text("탐험자 수")
+                                    .font(.caption2(.regular))
+                                    .foreground(LegacyColor.Label.alternative)
+                                
+                                Text("14.5%")
+                                    .font(.body2(.bold))
+                                    .foreground(LegacyColor.Label.normal)
                             }
                         }
-                        .foreground(LegacyColor.Primary.normal)
                         
-                        Text("( 302 )")
-                            .font(.caption2(.medium))
-                            .foreground(LegacyColor.Fill.alternative)
-                    }
-                    
-                    LegacyDivider()
-                    
-                    HStack(spacing: 20) {
-                        VStack(alignment: .leading) {
-                            Text("탐험자 수")
-                                .font(.caption2(.regular))
-                                .foreground(LegacyColor.Label.alternative)
-                            
-                            Text("200001명")
-                                .font(.body2(.bold))
-                                .foreground(LegacyColor.Label.normal)
-                        }
-                        
-                        VStack(alignment: .leading) {
-                            Text("탐험자 수")
-                                .font(.caption2(.regular))
-                                .foreground(LegacyColor.Label.alternative)
-                            
-                            Text("14.5%")
-                                .font(.body2(.bold))
-                                .foreground(LegacyColor.Label.normal)
+                        AnimationButton {
+                            flow.push(EmptyView())
+                        } label: {
+                            Text("한줄평 남기기")
+                                .font(.caption1(.bold))
+                                .foreground(LegacyColor.Label.netural)
+                                .padding(.horizontal, 6)
+                                .frame(maxWidth: .infinity)
+                                .frame(height: 30)
+                                .background(LegacyColor.Fill.normal)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 12)
+                                        .stroke(lineWidth: 2)
+                                        .foreground(LegacyColor.Line.netural)
+                                )
+                                .clipShape(size: 12)
                         }
                     }
                     
-                    AnimationButton {
-                        flow.push(EmptyView())
-                    } label: {
-                        Text("한줄평 남기기")
-                            .font(.caption1(.bold))
-                            .foreground(LegacyColor.Label.netural)
-                            .padding(.horizontal, 6)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 30)
-                            .background(LegacyColor.Fill.normal)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(lineWidth: 2)
-                                    .foreground(LegacyColor.Line.netural)
+                    if let url = URL(string: data.ruinsImage) {
+                        ZStack(alignment: .bottomLeading) {
+                            KFImage(url)
+                                .placeholder { _ in
+                                    RoundedRectangle(cornerRadius: 16)
+                                        .fill(Color.gray.opacity(0.3))
+                                        .frame(width: 160, height: 224)
+                                        .redacted(reason: .placeholder)
+                                        .shimmering()
+                                }
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 160, height: 224)
+                                .clipShape(size: 16)
+                            
+                            
+                            Text(data.name)
+                                .font(.bitFont(size: 14))
+                                .foreground(LegacyColor.Common.white)
+                                .padding(6)
+                                .clipShape(size: 8)
+                                .padding([.leading, .bottom], 8)
+                        }
+                        .overlay(
+                            RuinsCategory(
+                                category: data.category
                             )
-                            .clipShape(size: 12)
-                    }
-                }
-                
-                if let url = URL(string: data.ruinsImage) {
-                    ZStack(alignment: .bottomLeading) {
-                        KFImage(url)
-                            .placeholder { _ in
-                                RoundedRectangle(cornerRadius: 16)
-                                    .fill(Color.gray.opacity(0.3))
-                                    .frame(width: 160, height: 224)
-                                    .redacted(reason: .placeholder)
-                                    .shimmering()
-                            }
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 160, height: 224)
-                            .clipShape(size: 16)
-                        
-                        
-                        Text(data.name)
-                            .font(.bitFont(size: 14))
-                            .foreground(LegacyColor.Common.white)
-                            .padding(6)
-                            .clipShape(size: 8)
-                            .padding([.leading, .bottom], 8)
-                    }
-                    .overlay(
-                        RuinsCategory(
-                            category: data.category
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                            .padding([.leading, .top], 8),
+                            alignment: .topLeading
                         )
-                        .frame(maxWidth: .infinity, alignment: .topLeading)
-                        .padding([.leading, .top], 8),
-                        alignment: .topLeading
-                    )
+                    }
                 }
+                .padding(6)
             }
-            .padding(6)
-            
-            AnimationButton {
-                action()
-            } label: {
-                Text("퀴즈 풀고 탐험하기")
-                    .frame(maxWidth: .infinity)
-                    .font(.body1(.bold))
-                    .foreground(LegacyColor.Blue.netural)
-                    .padding(.vertical, 14)
-                    .background(LegacyColor.Fill.netural)
-                    .clipShape(size: 12)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(lineWidth: 2)
-                            .foreground(LegacyColor.Blue.netural)
-                    )
+            .overlay(alignment: .bottom) {
+                AnimationButton {
+                    action()
+                } label: {
+                    Text("퀴즈 풀고 탐험하기")
+                        .frame(maxWidth: .infinity)
+                        .font(.body1(.bold))
+                        .foreground(LegacyColor.Blue.netural)
+                        .padding(.vertical, 14)
+                        .background(LegacyColor.Fill.netural)
+                        .clipShape(size: 12)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(lineWidth: 2)
+                                .foreground(LegacyColor.Blue.netural)
+                        )
+                }
+                .padding(.top, 8)
             }
-            .padding(.top, 8)
+            .frame(height: 400)
         }
         .padding(8)
         .background(LegacyColor.Background.normal)
